ebd489c052ff96ab87ead19edb17c7cf5d60e940
Tomás Di Domenico <tdido@tdido.eu>
2022-09-08 11:49:15 +0200

    Get whitelist file using CI

--- .test/generate_data.sh
+++ .test/generate_data.sh
@@ -15,3 +15,6 @@ conda create -y -q -n bollito_prep star=2.7.1a
 conda activate bollito_prep
 mkdir data/ref/genome.chr19.fa_idx
 STAR --runMode genomeGenerate --genomeDir data/ref/genome.chr19.fa_idx --genomeFastaFiles data/ref/genome.chr19.fa --genomeSAindexNbases 11
+
+mkdir -p res/whitelists
+curl -L "https://github.com/10XGenomics/cellranger/blob/master/lib/python/cellranger/barcodes/translation/3M-february-2018.txt.gz?raw=true" | gunzip -c | cut -f1 > res/whitelists/3M-february-2018.txt
